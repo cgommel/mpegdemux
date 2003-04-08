7898694387b2682c714d100b961254b8bbd33be6
Hampa Hug <hampa@hampa.ch>
2003-04-08 23:20:59 +0000

    List skipped bytes.

--- src/mpeg_list.c
+++ src/mpeg_list.c
@@ -5,7 +5,7 @@
 /*****************************************************************************
  * File name:     mpeg_list.c                                                *
  * Created:       2003-02-02 by Hampa Hug <hampa@hampa.ch>                   *
- * Last modified: 2003-03-08 by Hampa Hug <hampa@hampa.ch>                   *
+ * Last modified: 2003-04-09 by Hampa Hug <hampa@hampa.ch>                   *
  * Copyright:     (C) 2003 by Hampa Hug <hampa@hampa.ch>                     *
  *****************************************************************************/
 
@@ -20,7 +20,7 @@
  * Public License for more details.                                          *
  *****************************************************************************/
 
-/* $Id: mpeg_list.c,v 1.11 2003/03/08 08:23:32 hampa Exp $ */
+/* $Id: mpeg_list.c,v 1.12 2003/04/08 23:20:59 hampa Exp $ */
 
 
 #include "config.h"
@@ -35,6 +35,32 @@
 #include "mpegdemux.h"
 
 
+static unsigned long long skip_ofs = 0;
+static unsigned long      skip_cnt = 0;
+
+
+static
+void mpeg_list_print_skip (FILE *fp)
+{
+  if (skip_cnt > 0) {
+    fprintf (fp, "%08llx: skip %lu\n", skip_ofs, skip_cnt);
+
+    skip_cnt = 0;
+  }
+}
+
+static
+int mpeg_list_skip (mpeg_demux_t *mpeg)
+{
+  if (skip_cnt == 0) {
+    skip_ofs = mpeg->ofs;
+  }
+
+  skip_cnt += 1;
+
+  return (0);
+}
+
 static
 int mpeg_list_system_header (mpeg_demux_t *mpeg)
 {
@@ -45,6 +71,8 @@ int mpeg_list_system_header (mpeg_demux_t *mpeg)
   }
 
   fp = (FILE *) mpeg->ext;
+
+  mpeg_list_print_skip (fp);
 
   fprintf (fp, "%08llx: system header[%lu]: size=%u fixed=%d csps=%d\n",
     mpeg->ofs, mpeg->shdr_cnt - 1,
@@ -73,6 +101,8 @@ int mpeg_list_packet (mpeg_demux_t *mpeg)
   }
 
   fp = (FILE *) mpeg->ext;
+
+  mpeg_list_print_skip (fp);
 
   if (mpeg->packet.type == 1) {
     type = "MPEG1";
@@ -105,6 +135,8 @@ int mpeg_list_pack (mpeg_demux_t *mpeg)
 
   fp = (FILE *) mpeg->ext;
 
+  mpeg_list_print_skip (fp);
+
   fprintf (fp, "%08llx: pack[%lu]: type=%u scr=%llu[%.4f] mux=%lu[%.2f] stuff=%u\n",
     mpeg->ofs, mpeg->pack_cnt - 1,
     mpeg->pack.type,
@@ -129,6 +161,8 @@ int mpeg_list_end (mpeg_demux_t *mpeg)
 
   fp = (FILE *) mpeg->ext;
 
+  mpeg_list_print_skip (fp);
+
   fprintf (fp, "%08llx: end\n", mpeg->ofs);
 
   return (0);
@@ -144,14 +178,20 @@ int mpeg_list (FILE *inp, FILE *out)
     return (1);
   }
 
+  skip_cnt = 0;
+  skip_ofs = 0;
+
   mpeg->ext = out;
 
+  mpeg->mpeg_skip = &mpeg_list_skip;
   mpeg->mpeg_system_header = &mpeg_list_system_header;
   mpeg->mpeg_pack = &mpeg_list_pack;
   mpeg->mpeg_packet = &mpeg_list_packet;
   mpeg->mpeg_end = &mpeg_list_end;
 
   r = mpegd_parse (mpeg);
+
+  mpeg_list_print_skip (out);
 
   mpeg_print_stats (mpeg, out);
 
