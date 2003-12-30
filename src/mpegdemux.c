92b3c9f88680afe6a67f69c6a1584445d9c41dc1
Hampa Hug <hampa@hampa.ch>
2003-12-30 10:52:35 +0000

    Added simple inversion for invalid streams.

--- src/mpegdemux.c
+++ src/mpegdemux.c
@@ -5,7 +5,7 @@
 /*****************************************************************************
  * File name:     mpegdemux.c                                                *
  * Created:       2003-02-01 by Hampa Hug <hampa@hampa.ch>                   *
- * Last modified: 2003-09-10 by Hampa Hug <hampa@hampa.ch>                   *
+ * Last modified: 2003-12-30 by Hampa Hug <hampa@hampa.ch>                   *
  * Copyright:     (C) 2003 by Hampa Hug <hampa@hampa.ch>                     *
  *****************************************************************************/
 
@@ -20,7 +20,7 @@
  * Public License for more details.                                          *
  *****************************************************************************/
 
-/* $Id: mpegdemux.c,v 1.19 2003/09/10 17:05:00 hampa Exp $ */
+/* $Id: mpegdemux.c,v 1.20 2003/12/30 10:52:35 hampa Exp $ */
 
 
 #include "config.h"
@@ -416,9 +416,21 @@ int main (int argc, char **argv)
         return (1);
       }
 
-      if (str_get_streams (argv[argi], par_invalid)) {
-        prt_err ("%s: bad stream id (%s)\n", argv[0], argv[argi]);
-        return (1);
+      if (strcmp (argv[argi], "-") == 0) {
+        for (i = 0; i < 256; i++) {
+          if (par_stream[i] & PAR_STREAM_EXCLUDE) {
+            par_invalid[i] &= ~PAR_STREAM_EXCLUDE;
+          }
+          else {
+            par_invalid[i] |= PAR_STREAM_EXCLUDE;
+          }
+        }
+      }
+      else {
+        if (str_get_streams (argv[argi], par_invalid)) {
+          prt_err ("%s: bad stream id (%s)\n", argv[0], argv[argi]);
+          return (1);
+        }
       }
     }
     else if (str_isarg2 (argv[argi], "-b", "--base-name")) {
